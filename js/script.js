e7604ceb5875d463656285fbc2a319ad369d3100
maicol <maicol.matteoli@gmail.com>
2021-06-20 12:48:07 +0200

    add : propietà color

--- js/script.js
+++ js/script.js
@@ -101,13 +101,56 @@ const icons = [
 
 // alert("ci sono");
 // let html = document.getElementById("contenuto");
-icons.forEach((element) => {
-    const {name , prefix, type, family} = element;
+icons.forEach((element) => { //scrive nella pagina tutte le icons
+    const {name , prefix, type, family, color} = element;
     document.getElementById("contenuto").innerHTML += `
         <div class="box">
             <i class="${family} ${prefix}${name}"></i>
             <div class="title">${name.toUpperCase()}</div>
         </div>
-
     `;
 });
+// icons.forEach((element) => {
+//   element.color = "red"; 
+// });
+//asegno un colore per ogni tipo 
+let coloredIcons = icons.map((element) => {
+  let c;
+  switch(element.type){
+    case "user" :
+      // console.log("è un utente");
+      c = "purple";
+      break;
+    case "vegetable" : 
+      c = "green";
+      // console.log("è una verdura");
+      break;
+    case "animal" :
+      c =  "orange";
+      // console.log("è un animale");
+      break;
+    default:
+      console.log("qualcosa non funziona, merda!");
+
+  }
+  return{ //pusho il colore determinato dallo switch
+    ...element, //prende tutti gli elementi (in questo caso "pusha" in coloredIcons un oggetto alla volta)
+    color : c,  // con questo aggiungo ad ogni oggetto questa proprietà
+   };
+})
+console.log(coloredIcons);
+// let coloredIcons = icons.map((element) => {
+//   return {
+//     ...element , color,
+//   }
+// });
+// console.log(coloredIcons);
+//utenti : viola
+//animali : blu
+//verdure : arancio
+
+//coloro le icone per tipo
+
+
+//return ...element , color 
+//ritorna un nuovo elemento con aggiunta la propietà color 
